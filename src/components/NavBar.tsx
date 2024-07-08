c24ed99b9ef38496ec16d9ce8f066239d74a37c0
Alish-Rawat <rawatalish4@gmail.com>
2024-07-08 08:32:17 +0530

    courses page

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -18,15 +18,17 @@ const NavBar = ({ className }: { className?: string }) => {
             item="Home"
           ></MenuItem>
         </Link>
-        <MenuItem setActive={setActive} active={active} item="Courses">
-          <div className="flex flex-col space-y-4 text-sm">
-            <HoveredLink href="/courses">All Courses</HoveredLink>
-            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
-            <HoveredLink href="/courses">Song Writing</HoveredLink>
-            <HoveredLink href="/courses">Music Production</HoveredLink>
-          </div>
-        </MenuItem>
+        <Link href={"/courses"}>
+          <MenuItem setActive={setActive} active={active} item="Courses">
+            <div className="flex flex-col space-y-4 text-sm">
+              <HoveredLink href="/courses">All Courses</HoveredLink>
+              <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+              <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+              <HoveredLink href="/courses">Song Writing</HoveredLink>
+              <HoveredLink href="/courses">Music Production</HoveredLink>
+            </div>
+          </MenuItem>
+        </Link>
         <Link href={"/contact"}>
           <MenuItem
             setActive={setActive}
